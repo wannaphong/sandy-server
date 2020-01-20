80b65c149c4db5b8edd1f8a1bbcce25bc2b3d85e
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-20 10:10:40 +0700

    Update Config.php

--- sandy/app/Config.php
+++ sandy/app/Config.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Config extends Model
 {
     //
+    public static function addConfigAssistant($data){
+        return DB::table('config_assistant')
+        ->insert($data);
+    }
+    public static function getconfig($aid){
+        return DB::table('config_assistant')->where('assistantid', '=', $aid)->first();
+    }
+    public static function getwificonfig($aid){
+        return DB::table('config_assistant')->where('assistantid', '=', $aid)->first()->wifi_config;
+    }
 }
