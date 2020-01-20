d9d3a8267fb13546a84f6ab1feac2bf1767c18a1
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-20 10:15:11 +0700

    Update Config.php

--- sandy/app/Config.php
+++ sandy/app/Config.php
@@ -15,7 +15,13 @@ class Config extends Model
     public static function getconfig($aid){
         return DB::table('config_assistant')->where('assistantid', '=', $aid)->first();
     }
+    public static function getprovince($aid){
+        return DB::table('config_assistant')->where('assistantid', '=', $aid)->first()->province;
+    }
     public static function getwificonfig($aid){
         return DB::table('config_assistant')->where('assistantid', '=', $aid)->first()->wifi_config;
+    }
+    public static function getlinenotify($aid){
+        return DB::table('config_assistant')->where('assistantid', '=', $aid)->first()->linenotify;
     }
 }
